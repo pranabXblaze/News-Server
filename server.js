ae09c8dd1576d1af98a92781d606c4863826435c
pranabXblaze <pranabkuamrs54@gmail.com>
2024-08-07 17:38:11 +0530

    Init news-server

---
+++ server.js
@@ -0,0 +1,65 @@
+import express, { urlencoded } from 'express';
+import cors from 'cors';
+import axios from 'axios';
+
+const api_key = "56e0a9701f0a440c99abf2f33a9110ad"
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']  // Additional headers to be included in the request
+}))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+async function fetchNews(url) {
+   try {
+    const response = await axios.get(url)
+    return {
+        status : 200,
+        message: "Succesfully fetched tha data.",
+        data : response.data
+    }
+   } catch (error) {
+    console.error(`API request Error : ${error.message}`);
+    return {
+        status : 500,
+        message: "Failed to fetch the data.",
+        error: error.response ? error.response.data : error.message
+  }
+ }
+}
+
+app.get('/all-news', async (req, res) => {
+    let q = req.query.q || "world";
+    let pageSize = parseInt(req.query.pageSize) || 80;
+    let page = parseInt(req.query.page) || 1;
+
+    const result = await fetchNews(`https://newsapi.org/v2/everything?page=${page}&pageSize=${pageSize}&q=${encodeURIComponent(q)}&apikey=${api_key}`)
+    res.status(result.status).json(result)
+})
+
+app.get("/top-headlines", async (req, res) => {
+    let pageSize = parseInt(req.query.pageSize) || 80;
+    let page = parseInt(req.query.page) || 1;
+    let category = req.query.category || "general";
+  
+   const result = await fetchNews(`https://newsapi.org/v2/top-headlines?category=${category}&language=en&page=${page}&pageSize=${pageSize}&apiKey=${api_key}`);
+   res.status(result.status).json(result);
+  });
+
+app.get("/country/:iso", async (req, res) => {
+    let pageSize = parseInt(req.query.pageSize) || 80;
+    let page = parseInt(req.query.page) || 1;
+    const country = req.params.iso || 'in'
+
+    const result = await fetchNews(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${api_key}&page=${page}&pageSize=${pageSize}`);
+    res.status(result.status).json(result);
+  });
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
